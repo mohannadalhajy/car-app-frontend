d74997ce7a000d770a51cc5db3134b2244a3d1d7
Muhannad <mohannad.alhajy@gmail.com>
2024-01-21 02:00:35 +0400

    delete counter

--- src/utils/redux/store.ts
+++ src/utils/redux/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterSlice from './features/counter/counterSlice'
 import carsSlice from './features/cars/carsSlice'
 
 export const makeStore = () => {
